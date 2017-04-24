d8c8b025eeda28c28d7e6f86fc13669784f38842
Ravliox <IstrateE@gmail.com>
2017-04-25 00:06:21 +0300

    finished

--- client.c
+++ client.c
@@ -17,9 +17,11 @@ void error(char *msg)
 
 int main(int argc, char *argv[])
 {
-    int sockfd, n;
+    int sockfd, n, SOCK_UTP;
     struct sockaddr_in serv_addr;
     struct hostent *server;
+    int last_login = -1;
+    int connected = 0;
 
     /* opening the log file */
     char id_name[30] = "client-";
@@ -38,6 +40,7 @@ int main(int argc, char *argv[])
     }  
     
 	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+    SOCK_UTP = socket (AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) 
         error("ERROR opening socket");
     
@@ -47,28 +50,43 @@ int main(int argc, char *argv[])
     
     
     if (connect(sockfd,(struct sockaddr*) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");    
+        error("ERROR connecting");     
     
     while(1){
   		//citesc de la tastatura
     	memset(buffer, 0 , BUFLEN);
     	fgets(buffer, BUFLEN-1, stdin);
-
+        fprintf (log_file, "%s", buffer);
+        
+        //daca trebuie sa semnalam serverului ca ne deconectam
         if (strcmp (buffer, "quit\n") == 0){
             memset (buffer, 0, BUFLEN);
             sprintf (buffer, "%s", "Quitting");
             send(sockfd,buffer,strlen(buffer), 0);
             break;
         }
+        // daca cerem unlock
+        else if (strcmp (buffer, "unlock\n") == 0){
+            char* last_login_string = malloc (20);
+            sprintf (last_login_string, " %d", last_login);
+            sprintf (buffer, "%s", "unlock");
+            strcat (buffer, last_login_string);
+        }
 
     	//trimit mesaj la server
     	n = send(sockfd,buffer,strlen(buffer), 0);
+        char *token = strtok (buffer, " ");         // daca ce am trimis este o cerere de login
+        if (strcmp (token, "login") == 0){          // salveaza numarul cardului
+            token = strtok (NULL, " ");
+            last_login = atoi (token);
+              
+        }
     	if (n < 0) 
         	 error("ERROR writing to socket");
         memset (buffer, 0, BUFLEN);
-        n = recv (sockfd, buffer, sizeof(buffer), 0);
-        fprintf (log_file, "ATM> %s\n", buffer);
-        printf ("ATM> %s\n", buffer);
+        n = recv (sockfd, buffer, sizeof(buffer), 0);   //primeste raspuns si afiseaza
+        fprintf (log_file, "%s\n\n", buffer);
+        printf ("%s\n", buffer);
     }
 
     fclose (log_file);
--- selectserver.c
+++ selectserver.c
@@ -7,6 +7,7 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 
+//structura in care ne vom tine informatiile despre clienti
 typedef struct {
 	char fname[12];
 	char lname[12];
@@ -28,6 +29,7 @@ void error(char *msg)
     exit(1);
 }
 
+// functia cu care vom identifica userul ce detine numarul de card dat
 int find_card (client* client_list, int user_count, int number){
 	for (int i = 0; i < user_count; i++){
 		if (client_list[i].card_no == number){
@@ -36,7 +38,7 @@ int find_card (client* client_list, int user_count, int number){
 	}
 	return -1;
 }
-
+// functia cu care vom identifica userul ce tocmai a introdus pinul
 int find_pin (client* client_list, int user_count, int pin){
 	for (int i = 0; i < user_count; i++){
 		if (client_list[i].pin == pin){
@@ -46,6 +48,7 @@ int find_pin (client* client_list, int user_count, int pin){
 	return -1;
 }
 
+// functia ce identifica ce user avem pe socketul curent
 int find_socket (client* client_list, int user_count, int socket){
 	for (int i = 0; i < user_count; i++){
 		if (client_list[i].socket == socket){
@@ -62,15 +65,19 @@ int main(int argc, char *argv[])
 	 char send_buffer[BUFLEN];
      struct sockaddr_in serv_addr, cli_addr;
      int n, i, j, user_count, k;
-
-	 FILE* users_data_file = fopen ("users_data_file", "r");
+	 
+	 //deschidem fisierul cu useri
+	 FILE* users_data_file = fopen (argv[2], "r");
 	 char* line = malloc (sizeof(char) * 100);
-	 fgets (line, 100, users_data_file);
+	 fgets (line, 100, users_data_file);	// citim numarul de useri
 	 k = 0;
+
 	 user_count = atoi (line);
-	 client* client_list = malloc (sizeof(client) * user_count);
+	 client* client_list = malloc (sizeof(client) * user_count);	//alocam vectorul de clienti
+
 	 while (k < user_count){
 		fgets (line, 100, users_data_file);
+		// transpune tot textul intr-un dublu array
 		char** data = malloc (sizeof (char*) * 6);
 		for (int p = 0; p < 6; p++){
 			data[p] = malloc (sizeof(char) * 20);
@@ -82,6 +89,8 @@ int main(int argc, char *argv[])
 			p++;
 			token = strtok (NULL, " ");
 		}
+
+		// prelucreaza informatia din text in structura si adaug-o la vector
 		strcpy (client_list[k].fname, data[0]);
 		strcpy (client_list[k].lname, data[1]);
 		client_list[k].card_no = atoi (data[2]);
@@ -94,16 +103,7 @@ int main(int argc, char *argv[])
 		k++; 
 	 }
 
-	 for (int p = 0; p < user_count; p++){
-		 printf ("%s ", client_list[p].fname);
-		 printf ("%s ", client_list[p].lname);
-		 printf ("%d ", client_list[p].card_no);
-		 printf ("%d ", client_list[p].pin);
-		 printf ("%s ", client_list[p].secret_pass);
-		 printf ("%f ", client_list[p].sold);
-		 printf ("%d\n", client_list[p].login);
-	 }
-
+	 
      fd_set read_fds;	//multimea de citire folosita in select()
      fd_set tmp_fds;	//multime folosita temporar 
      int fdmax;		//valoare maxima file descriptor din multimea read_fds
@@ -139,9 +139,8 @@ int main(int argc, char *argv[])
      //adaugam noul file descriptor (socketul pe care se asculta conexiuni) in multimea read_fds
      FD_SET(sockfd, &read_fds);
      fdmax = sockfd;
-
      // main loop
-	while (1) {
+	 while (1) {
 		tmp_fds = read_fds; 
 		if (select(fdmax + 1, &tmp_fds, NULL, NULL, NULL) == -1) 
 			error("ERROR in select");
@@ -182,130 +181,147 @@ int main(int argc, char *argv[])
 					} 
 					
 					else { //recv intoarce >0
-						printf ("Am primit de la clientul de pe socketul %d, mesajul: %s\n", i, buffer);
 						memset(send_buffer, 0 , BUFLEN);
 						char* tokens = strtok (buffer, " ");
+						// citim primul cuvant
 						if (strcmp (tokens, "login") == 0){
-							printf ("%d\n", strlen (tokens));
-							tokens = strtok (NULL, " ");
+							
+							tokens = strtok (NULL, " ");	//luam numarul de card
 							int nr_card = atoi(tokens);
-							tokens = strtok (NULL, " ");
+							tokens = strtok (NULL, " ");	//luam pinul
 							int pin = atoi (tokens);
 							printf ("PIN: %d\n", pin);
-							int find = find_card (client_list, user_count, nr_card);
-							if (find < 0){
-								sprintf (send_buffer, "%s", "-4 : Numar card inexistent");
+							int find = find_card (client_list, user_count, nr_card);	// gaseste cardul
+							if (find < 0){												//daca nu il gasesti
+								sprintf (send_buffer, "%s", "ATM> -4 : Numar card inexistent");
 								send (i, send_buffer, strlen(send_buffer), 0);
 							}
-							else if (client_list[find].login == 0){
+							else if (client_list[find].login == 0){						//daca il gasesti
+																						//verifica daca exista ses. deschisa
 								int pin_find = find_pin (client_list, user_count, pin);
-								if (pin_find < 0){
+								if (pin_find < 0){										// verifica pinul
 									if (client_list[find].counter == 3){
 										client_list[find].login = 2;
-										sprintf (send_buffer, "%s", "-5 : Card Blocat");
+										sprintf (send_buffer, "%s", "ATM> -5 : Card Blocat");		//blocheaza cardul
 										send (i, send_buffer, strlen(send_buffer), 0);
 									}
 									else {
-										sprintf (send_buffer, "%s", "-3 : Pin incorect");
+										sprintf (send_buffer, "%s", "ATM> -3 : Pin incorect");
 										send (i, send_buffer, strlen(send_buffer), 0);
 										client_list[find].counter++;
 									}
 								}
-								else {
-									sprintf (send_buffer, "Welcome, %s %s!", client_list[find].fname, client_list[find].lname);
+								else {				// succes
+									sprintf (send_buffer, "ATM> Welcome, %s %s!", client_list[find].fname, client_list[find].lname);
 									send (i, send_buffer, strlen(send_buffer), 0);
 									client_list[find].login = 1;
 									client_list[find].socket = i;
 								}
 							} 
-							else if (client_list[find].login == 1){
-								sprintf (send_buffer, "%s", "-2 : Sesiune deja deschisa");
+							else if (client_list[find].login == 1){					//daca sesiunea e deschisa
+								sprintf (send_buffer, "%s", "ATM> -2 : Sesiune deja deschisa");
 								send (i, send_buffer, strlen (send_buffer), 0);
 							}
-							else if (client_list[find].login == 2){
-								sprintf (send_buffer, "%s", "-5 : Card blocat");
+							else if (client_list[find].login == 2){					//daca cardul e blocat
+								sprintf (send_buffer, "%s", "ATM> -5 : Card blocat");
 								send (i, send_buffer, strlen (send_buffer), 0);
 							}
 						}
-						else if (strcmp (tokens, "logout\n") == 0){
+						else if (strcmp (tokens, "logout\n") == 0){					// pentru logout
 							int socket_find = find_socket (client_list, user_count, i);
-							if (socket_find >= 0){
+							if (socket_find >= 0){									// daca este autentificat deja
 								client_list[socket_find].socket = -1;
 								client_list[socket_find].login = 0;
-								sprintf (send_buffer, "%s", "Deconectare de la bancomat!");
+								sprintf (send_buffer, "%s", "ATM> Deconectare de la bancomat!");
 								send (i, send_buffer, strlen (send_buffer), 0);
 							}
-							else {
+							else {													// daca nu
 								printf ("%d %d \n", socket_find, client_list[socket_find].socket);
-								sprintf (send_buffer, "%s", "-1 : Clientul nu este autentificat");
+								sprintf (send_buffer, "%s", "ATM> -1 : Clientul nu este autentificat");
 								send (i, send_buffer, strlen (send_buffer), 0);
 							}
 						}
-						else if (strcmp (tokens, "listsold\n") == 0){
-							int socket_find = find_socket (client_list, user_count, i);
+						else if (strcmp (tokens, "listsold\n") == 0){				// pentru sold
+							int socket_find = find_socket (client_list, user_count, i);	//identifica userul
 							if (socket_find >= 0){
-								sprintf (send_buffer, "%.2f", client_list[socket_find].sold);
+								sprintf (send_buffer, "ATM> %.2f", client_list[socket_find].sold);
 								send (i, send_buffer, strlen (send_buffer), 0);
 							}
 							else{
-								sprintf (send_buffer, "%s", "-1 : Clientul nu este autentificat");
+								sprintf (send_buffer, "%s", "ATM> -1 : Clientul nu este autentificat");
 								send (i, send_buffer, strlen (send_buffer), 0);
 							}
 						}
-						else if (strcmp (tokens, "getmoney") == 0){
+						else if (strcmp (tokens, "getmoney") == 0){					//pentru getmoney
 							int socket_find = find_socket (client_list, user_count, i);
 							if (socket_find >= 0){
 								tokens = strtok (NULL, " ");
 								int sum = atoi (tokens);
-								if (sum % 10 != 0){
-									sprintf (send_buffer, "%s", "-9 : Suma nu este multiplu de 10");
+								if (sum % 10 != 0){									//verificam conditiile
+									sprintf (send_buffer, "%s", "ATM> -9 : Suma nu este multiplu de 10");
 									send (i, send_buffer, strlen (send_buffer), 0);
 								}
 								else if (sum > client_list[socket_find].sold){
-									sprintf (send_buffer, "%s", "-8 : Fonduri insuficiente");
+									sprintf (send_buffer, "%s", "ATM> -8 : Fonduri insuficiente");
 									send (i, send_buffer, strlen (send_buffer), 0);
 								}
 								else {
 									client_list[socket_find].sold -= sum;
-									sprintf (send_buffer, "Suma %d retrasa cu succes", sum);
+									sprintf (send_buffer, "ATM> Suma %d retrasa cu succes", sum);
 									send (i, send_buffer, strlen (send_buffer), 0); 
 								}
 							}
-							else{
-								sprintf (send_buffer, "%s", "-1 : Clientul nu este autentificat");
+							else{													//daca nu este autentificat
+								sprintf (send_buffer, "%s", "ATM> -1 : Clientul nu este autentificat");
 								send (i, send_buffer, strlen (send_buffer), 0);
 							}
 						}
-						else if (strcmp (tokens, "putmoney") == 0){
+						else if (strcmp (tokens, "putmoney") == 0){					//pentru putmoney
 							int socket_find = find_socket (client_list, user_count, i);
 							if (socket_find >= 0){
 								tokens = strtok (NULL, " ");
 								float sum = atof (tokens);
 								client_list[socket_find].sold += sum;
-								sprintf (send_buffer, "%s", "Suma depusa cu succes");
+								sprintf (send_buffer, "%s", "ATM> Suma depusa cu succes");
 								send (i, send_buffer, strlen (send_buffer), 0);
 							}
 							else{
-								sprintf (send_buffer, "%s", "-1 : Clientul nu este autentificat");
+								sprintf (send_buffer, "%s", "ATM> -1 : Clientul nu este autentificat");
 								send (i, send_buffer, strlen (send_buffer), 0);
 							}
 						}
-						else if (strcmp (tokens, "Quitting") == 0){
+						else if (strcmp (tokens, "Quitting") == 0){				//pentru quit
 							int socket_find = find_socket (client_list, user_count, i);
-							if (socket_find >= 0){
-								client_list[socket_find].socket = -1;
+							if (socket_find >= 0){								// daca e logat 
+								client_list[socket_find].socket = -1;			//delogheaza-l
 								client_list[socket_find].login = 0;
 							}
-							printf("Client disconnected", i);
+							printf("Client disconnected", i);					//sterge-l din lista
 							close(i); 
-							FD_CLR(i, &read_fds);
+							FD_CLR(i, &read_fds);	
 
 						}
-						else {
-							printf ("\n");
-							printf ("%d\n", strlen (tokens));
-							printf ("%d\n", strcmp (tokens, "logout\n") );
-							printf ("EEELSE\n");
+						else if (strcmp (tokens, "unlock") == 0){				//pentru unlock
+							tokens = strtok (NULL, " ");
+							int card_to_unlock = atoi (tokens);
+							
+							int unlock_find = find_card (client_list, user_count, card_to_unlock);		//gaseste cardul
+							printf ("%d %d\n", card_to_unlock, unlock_find);
+							sprintf (send_buffer, "%s", "UNLOCK> Trimite parola secreta");				//cere parola
+							send (i, send_buffer, strlen (send_buffer), 0);
+							memset(buffer, 0 , BUFLEN);
+							memset(send_buffer, 0 , BUFLEN);
+							recv(i, buffer, sizeof(buffer), 0);											//primeste parola
+							tokens = strtok (buffer, "\n");
+							if ( strcmp (buffer, client_list[unlock_find].secret_pass) != 0 ){			//esuare
+								sprintf (send_buffer, "%s", "UNLOCK > Deblocare esuata");
+								send (i, send_buffer, strlen (send_buffer), 0);
+							}
+							else {
+								sprintf (send_buffer, "%s", "UNLOCK > Client deblocat");				//succes
+								client_list[unlock_find].login = 0;
+								send (i, send_buffer, strlen (send_buffer), 0);
+							}
 						}
 					}					
 				}
